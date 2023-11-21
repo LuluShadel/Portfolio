88b63d144732472091f0b703d94c989e741a7339
Lucile THIEULIN <lucile.thieulin96@gmail.com>
2023-11-21 09:15:16 +0100

    ajout des polices google font

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const Home = () => {
     <>
       <Head>
         <title>Lucile Portfolio</title>
-        <meta name="description" content="Joel's Portfolio" />
+        <meta name="description" content="Lucile's Portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
