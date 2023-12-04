c3280a606dc7dc2eb9d5ee1bb201fcda5779d446
Lucile THIEULIN <lucile.thieulin96@gmail.com>
2023-12-04 15:55:01 +0100

    mise en pace de i18n et ajout du select pour le langage

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,6 +7,11 @@ import Link from "next/link";
 import logo from "../../public/logo_git_hub.png"
 import Line from "../../public/Line.png"
 
+
+
+import { useTranslation } from 'next-i18next';
+
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faAt,
@@ -18,7 +23,14 @@ import {
 
 
 
+
+
 const Header = () => {
+
+  const { t  } = useTranslation();
+
+  
+
   return (
     <div className={styles['header-container']}>
       <div className={styles['triangle-container']}>
@@ -31,10 +43,14 @@ const Header = () => {
           <h1>Lucile Thieulin Portfolio</h1>
           <Image src={Line} alt="ligne orange" className={styles["line"]} />
       </div>
+        
+      
+
+
       <div className={styles["navBar"]}>
         <Link className={styles["navBar_link"]} href="/#Hero">
         <FontAwesomeIcon icon={faUser} className={styles["navBar_icon"]}/>
-          <p>About</p>
+          <p>{t("common:nav1")}</p>
         </Link>
         <Link className={styles["navBar_link"]} href="/#Projets">
         <FontAwesomeIcon icon={faLightbulb} className={styles["navBar_icon"]}/>
@@ -49,5 +65,6 @@ const Header = () => {
     </div>
   )
 }
+
 
 export default Header;
